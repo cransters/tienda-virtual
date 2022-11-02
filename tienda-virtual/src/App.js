feb41507e16639632c45918b28d84fdfefa3a41b
cransters <108357630+cransters@users.noreply.github.com>
2022-11-01 21:13:23 -0500

    tienda

---
+++ tienda-virtual/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Header } from "./componentes/Header";
+import { ProductosLista } from "./componentes/Productos/index";
+import 'boxicons';
+import { BrowserRouter as Router } from "react-router-dom";
+import {Paginas} from "./componentes/Paginas";
+import { DataProvider } from "./context/Dataprovider";
+
+function App() {
+  return (
+    <DataProvider>
+    <div className="App">
+      <Router>
+      <Header />
+      <Paginas />
+      </Router>
+    </div>
+    </DataProvider>
+  );
+}
+
+export default App;
